e457b44bdc6971bc3fbf6e4ebac583d9def07555
Shailesh Dhandhukiya <shaileshdhandhukiya012@gmail.com>
2026-01-01 23:37:37 +0530

    Frontend push

---
+++ public/build/assets/app-layout-Dt9qo_ia.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-CQMROZin.js";import{f as s}from"./app-sidebar-layout-Bod0TseD.js";const i=({children:o,breadcrumbs:p,...t})=>r.jsx(s,{breadcrumbs:p,...t,children:o});export{i as A};
