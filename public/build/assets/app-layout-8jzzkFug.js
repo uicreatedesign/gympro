bbde7ee105c7ce06bb4fc9f029620555581c7364
Shailesh Dhandhukiya <shaileshdhandhukiya012@gmail.com>
2026-01-03 23:58:30 +0530

    ADD : BUild added

---
+++ public/build/assets/app-layout-8jzzkFug.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-Br_IyjXx.js";import{n as s}from"./app-sidebar-layout-BJ3dFsrI.js";const i=({children:o,breadcrumbs:p,...t})=>r.jsx(s,{breadcrumbs:p,...t,children:o});export{i as A};
