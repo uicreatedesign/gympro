18eb633a715a8a0519cbaca8d25cee36a81e1550
shaileshdhandhukiya <shaileshdhandhukiya012@gmail.com>
2026-01-08 20:32:56 +0530

    ADD : Plan features/facility make it dynamic from static

--- public/build/assets/app-layout-BFJO53RA.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-BsguprTH.js";import{l as s}from"./app-sidebar-layout-2tr6FukO.js";const i=({children:o,breadcrumbs:p,...t})=>r.jsx(s,{breadcrumbs:p,...t,children:o});export{i as A};
---
+++ public/build/assets/clock-t0Uq0N4L.js
@@ -0,0 +1,1 @@
+import{e as c}from"./app-DA8P16aJ.js";const o=[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["polyline",{points:"12 6 12 12 16 14",key:"68esgv"}]],i=c("Clock",o);export{i as C};
